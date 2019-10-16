9af64a28564d4e3200fbb6f31f5bf529589c8159
BurningChuck <54845547+BurningChuck@users.noreply.github.com>
2019-10-16 09:20:42 +0300

    Update MyCircularList.h

--- 1Lab/first/MyCircularList.h
+++ 1Lab/first/MyCircularList.h
@@ -3,22 +3,28 @@
 #include "MyList.h"
 #include <string>
 
+// циклический список
 template <typename E>
 class MyCircularList: public MyList<E> {
 private:
+  // список, который оборачиваем
   MyList<E> *myList;
+  // текущий индекс
   int current;
 
 public:
+  // конструктор
   MyCircularList(MyList<E> *t) : MyList<E>() {
     myList = t;
     current = 0;
   }
 
+  // каждый раз возвращает следующий элемент, после последнего - первый
   E next() {
     return myList->get(current++ % myList->getSize());
   }
 
+  // остальные методы проксируют внутренний список
   void add(E element)    {        myList->add(element);     }
   E get(int index)       { return myList->get(index);       }
   E remove(int index)    { return myList->remove(index);    }
@@ -26,7 +32,7 @@ public:
   int indexOf(E element) { return myList->indexOf(element); }
   std::string toString() { return myList->toString();       }
 
-  int indexOfFirst(bool (*p)(E)) {
-    return myList->indexOfFirst(p);
-  }
+  void forEach(void (*b)(E)) { myList->forEach(b); }
+
+  int indexOfFirst(bool (*p)(E)) { return myList->indexOfFirst(p); }
 };
