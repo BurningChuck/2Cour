00089c642a06bd6fc96ee223394791226e4dee45
BurningChuck <54845547+BurningChuck@users.noreply.github.com>
2019-10-16 09:24:47 +0300

    Update Pentagon.h

--- 1Lab/second/Pentagon.h
+++ 1Lab/second/Pentagon.h
@@ -3,8 +3,11 @@
 #include "Shape.h"
 #include <cstdlib>
 
+// класс пятиугольника
+//   наследуемся от родительского класса Shape с 5 углами/рёбрами
 class Pentagon: public Shape<5> {
 public:
+  // статический метод для создания пятиугольника со случайными точками
   static Pentagon random() {
      return Pentagon(
        rand() % 100, rand() % 100,
@@ -15,6 +18,7 @@ public:
      );
   }
 
+  // конструктор по 5ти точкам
   Pentagon(
     double x1, double y1,
     double x2, double y2,
@@ -22,6 +26,7 @@ public:
     double x4, double y4,
     double x5, double y5
   ) : Shape() {
+    // заносим их в массив точек в родительском классе
     vertices[0][0] = x1;
     vertices[0][1] = y1;
     vertices[1][0] = x2;
@@ -34,17 +39,24 @@ public:
     vertices[4][1] = y5;
   }
 
+  // является ли пятиугольник правильным
   bool isRegularPentagon() {
+    // если стороны не одной длины - сразу нет
     if (!isHalfRegular()) return false;
 
+    // считаем диагональ из первой точки
     double dx = vertices[(1 + 2) % 5][0] - vertices[1][0];
     double dy = vertices[(1 + 2) % 5][1] - vertices[1][1];
     double diagonal = sqrt(dx * dx + dy * dy);
 
+    // для каждой следующей точки
     for (int i = 1; i < 5; i++) {
+      // считаем диагональ
       dx = vertices[(i + 2) % 5][0] - vertices[i][0];
       dy = vertices[(i + 2) % 5][1] - vertices[i][1];
+      // если её длина отличается от первой
       if (fabs(sqrt(dx * dx + dy * dy) - diagonal) < 0.001) {
+        // то пятиугольник не является правильным
         return false;
       }
     }
