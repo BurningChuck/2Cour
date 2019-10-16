be7834cddf8bd49279eaae6cab6fb3edd1fad2e1
BurningChuck <54845547+BurningChuck@users.noreply.github.com>
2019-10-16 09:26:36 +0300

    Update Shape.h

--- 1Lab/second/Shape.h
+++ 1Lab/second/Shape.h
@@ -4,39 +4,29 @@
 #include <sstream>
 #include <cmath>
 
+// общий родительский класс для всех фигур
+//   N - кол-во углов/рёбер
 template <int N>
 class Shape {
 protected:
+  // двумерный массив точек в виде
+  //   [[x1, y1], [x2, y2], ..., [xN, yN]]
   double vertices[N][2];
 
+  // метод для получения длин всех рёбер
   void edges(double ret[N]) {
+    // проходим по каждой точке
     for (int i = 0; i < N; i++) {
+      // считаем разницу координат i-ой и (i+1)-ой точек
+      //   (берём по модулю чтобы последняя (i+1)-ая точка была 0)
       double dx = vertices[(i + 1) % N][0] - vertices[i][0];
       double dy = vertices[(i + 1) % N][1] - vertices[i][1];
+      // считаем корень и записываем результат в выходной массив
       ret[i] = sqrt(dx * dx + dy * dy);
     }
   }
 
-public:
-  double perimeter() {
-    double ret = 0.0, e[N];
-    edges(e);
-    for (int i = 0; i < N; i++) {
-      ret += e[i];
-    }
-    return ret;
-  }
-
-  // https://en.wikipedia.org/wiki/Shoelace_formula
-  double area() {
-    double ret = 0.0;
-    for (int i = 0; i < N; i++) {
-      ret += vertices[i][0] * vertices[(i + 1) % N][1];
-      ret -= vertices[(i + 1) % N][0] * vertices[i][1];
-    }
-    return 0.5 * fabs(ret);
-  }
-
+  // метод для проверки являются ли все рёбра одинаковыми
   bool isHalfRegular() {
     double e[N];
     edges(e);
@@ -48,6 +38,32 @@ public:
     return true;
   }
 
+public:
+  // периметр
+  double perimeter() {
+    double ret = 0.0, e[N];
+    // получаем рёбра
+    edges(e);
+    // суммируем
+    for (int i = 0; i < N; i++) {
+      ret += e[i];
+    }
+    // возвращаем результат
+    return ret;
+  }
+
+  // площадь по формуле
+  //   https://en.wikipedia.org/wiki/Shoelace_formula
+  double area() {
+    double ret = 0.0;
+    for (int i = 0; i < N; i++) {
+      ret += vertices[i][0] * vertices[(i + 1) % N][1];
+      ret -= vertices[(i + 1) % N][0] * vertices[i][1];
+    }
+    return 0.5 * fabs(ret);
+  }
+
+  // метод для получения строкового представления фигуры по точкам
   std::string toString() {
     std::stringstream ret;
     ret << "(";
